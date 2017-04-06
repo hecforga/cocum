b7bc1cf2f572c467e5a6256d088c330527520563
Héctor Fornes <hctrfg@gmail.com>
2017-04-06 16:09:27 +0200

    Use black example image instead of green

--- app/imports/api/queries/queries.js
+++ app/imports/api/queries/queries.js
@@ -9,8 +9,8 @@ Queries.insert({
 });
 
 Queries.insert({
-  imageUrl : "http://res.cloudinary.com/ddjzq70ve/image/upload/v1491210829/9237500500_2_1_2.jpg",
-  croppedImageUrl : "http://res.cloudinary.com/ddjzq70ve/image/upload/v1491224013/9237500500_2_1_2_CROPPED.png",
+  imageUrl : "http://res.cloudinary.com/ddjzq70ve/image/upload/v1491210829/5239555800_2_1_2.jpg",
+  croppedImageUrl : "http://res.cloudinary.com/ddjzq70ve/image/upload/v1491224013/5239555800_2_1_2_CROPPED.png",
   results : []
 });
 
