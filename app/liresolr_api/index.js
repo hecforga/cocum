e1ece4cfa6a03daeba9ec7e2ade890f9258e998d
Héctor Fornes <hctrfg@gmail.com>
2017-08-24 16:58:52 +0200

    Use cloudinar to increase brightness of cropped image

--- app/liresolr_api/index.js
+++ app/liresolr_api/index.js
@@ -1,7 +1,8 @@
 const LIRESOLR_SERVER_URL = 'http://54.93.254.52:8983/solr/';
 
 export const fetchResults = (gender, category, imageUrl) => {
-  const url = LIRESOLR_SERVER_URL + gender + '_' + category + '/lireq?url=' + imageUrl + '&field=ce&ms=false&rows=12';
+  const cloudinaryUrl = 'http://res.cloudinary.com/ddjzq70ve/image/fetch/e_brightness_hsb:20/';
+  const url = LIRESOLR_SERVER_URL + gender + '_' + category + '/lireq?url=' + cloudinaryUrl + imageUrl + '&field=ce&ms=false&rows=12';
 
   return fetch(url).then((response) => {
     const docs = JSON.parse(response._bodyText).docs;
