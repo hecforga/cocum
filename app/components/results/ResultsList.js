766be1f320d74258b660d91912aa0d9227ba60d9
Héctor Fornes <hctrfg@gmail.com>
2017-09-28 12:31:36 +0200

    Use FlatList instead of ScrollView in ResultsList

--- app/components/results/ResultsList.js
+++ app/components/results/ResultsList.js
@@ -1,41 +1,45 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, TouchableHighlight, ScrollView, Image, Text, Dimensions } from 'react-native';
+import { StyleSheet, View, FlatList, Dimensions } from 'react-native';
 
 import ProductThumbnail from '../common/ProductThumbnail.js';
 
 const CONTAINER_PADDING = 16;
 const PRODUCT_THUMBNAIL_CONTAINER_MARGIN = 8;
 
-class ResultsList extends Component {
+class ResultsList1 extends Component {
   componentWillMount() {
     const { height, width } = Dimensions.get('window');
     this.imageWidth = (width - 2 * CONTAINER_PADDING - 4 * PRODUCT_THUMBNAIL_CONTAINER_MARGIN) / 2;
+    console.log('imageWidth: ' + this.imageWidth);
   }
 
   render() {
     const { productsInArraysOf2, onProductPress } = this.props;
 
     return (
-      <ScrollView style={styles.container}>
-        {productsInArraysOf2.map((arrayOf2Products, index) =>
-          <View
-            key={index}
-            style={styles.listRow}
-          >
-            {arrayOf2Products.map((product) =>
-              <ProductThumbnail
-                key={ product.productId}
-                product={product}
-                productThumbnailContainerStyle={{
-                  margin: PRODUCT_THUMBNAIL_CONTAINER_MARGIN,
-                  width: this.imageWidth
-                }}
-                onPress={onProductPress}
-              />
-            )}
-          </View>
-        )}
-      </ScrollView>
+      <FlatList
+        style={styles.container}
+        data={productsInArraysOf2}
+        keyExtractor={(item, index) => index}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.listRow}>
+              {item.map((product) =>
+                <ProductThumbnail
+                  key={ product.productId}
+                  product={product}
+                  productThumbnailContainerStyle={{
+                    margin: PRODUCT_THUMBNAIL_CONTAINER_MARGIN,
+                    width: this.imageWidth
+                  }}
+                  onPress={onProductPress}
+                />
+              )}
+            </View>
+          );
+        }}
+        initialNumToRender={2}
+      />
     );
   }
 }
@@ -57,4 +61,4 @@ const styles = StyleSheet.create({
   }
 });
 
-export default ResultsList;
+export default ResultsList1;
