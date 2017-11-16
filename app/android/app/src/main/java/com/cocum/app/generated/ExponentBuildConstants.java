0be783ccc01b10125d37b221a1c825045b58c654
Héctor Fornes <hctrfg@gmail.com>
2017-11-16 12:19:46 +0100

    Update gradle

--- app/android/app/src/main/java/com/cocum/app/generated/ExponentBuildConstants.java
+++ app/android/app/src/main/java/com/cocum/app/generated/ExponentBuildConstants.java
@@ -3,6 +3,6 @@ package com.cocum.app.generated;
 // This file is auto-generated. Please don't rename!
 public class ExponentBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "expa9f81069b8f942429303cbbafaa7e301://localhost:80";
+  public static final String DEVELOPMENT_URL = "expa9f81069b8f942429303cbbafaa7e301://3n-zcc.hecforga.app.exp.direct:80";
 
 }
