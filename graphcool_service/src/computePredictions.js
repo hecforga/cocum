4b40b41dc308d0b1c0a08acd950dcbd2d6c865a3
Héctor Fornes <hctrfg@gmail.com>
2017-12-21 13:05:43 +0100

    Create graphcool functions

---
+++ graphcool_service/src/computePredictions.js
@@ -0,0 +1,52 @@
+import * as fetch from 'isomorphic-fetch';
+
+const LAMBDA_URL = 'https://zvvi3r4gdh.execute-api.eu-central-1.amazonaws.com/dev/prediction';
+
+const categoriesInfo = {
+  prueba: {
+    properties: ['fit', 'length', 'neck', 'print', 'sleeve']
+  }
+};
+
+export default async event => {
+  try {
+    const { gender, category, imageUrl } = event.data;
+
+    const tags = fetchPredictions(gender, category, imageUrl);
+
+    return { data: { tags: await tags } };
+  } catch (e) {
+    return { error: e.message };
+  }
+};
+
+async function fetchPredictions(gender, category, imageUrl) {
+  let tags = {};
+  if (categoriesInfo[category]) {
+    const properties = categoriesInfo[category].properties;
+    await Promise.all(properties.map(async (property) => {
+      tags[property] = await fetchPrediction(category, property, imageUrl);
+    }));
+  }
+  return tags;
+}
+
+async function fetchPrediction(category, property, imageUrl) {
+  const body = {
+    data: {
+      category,
+      property,
+      imageUrl
+    }
+  };
+  return fetch(LAMBDA_URL, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": 'application/json'
+    }
+  })
+    .then(response => response.json()
+      .then(json => (json['data'] && json['data']['prediction']) || 'undefined')
+  );
+}
---
+++ graphcool_service/src/computeResults.js
@@ -0,0 +1,119 @@
+import * as fetch from 'isomorphic-fetch';
+var isEmpty = require('lodash.isempty');
+
+const LIRESOLR_SERVER_URL = 'http://34.242.219.160/solr/';
+
+const categoriesInfo = {
+  prueba: {
+    boosts: { fit: 10, length: 10, neck: 10, print: 10, sleeve: 10 },
+    maxScore: 50.0
+  }
+};
+
+export default async event => {
+  try {
+    const { mode, gender, category, imageUrl, productId, tags, filters } = event.data;
+
+    const visualResults = fetchVisualResults(mode, gender, category, imageUrl, productId, filters);
+
+    const textResults = fetchTextResults(mode, gender, category, tags, filters);
+
+    const mixedResults = mixResults(await visualResults, await textResults, category);
+
+    return { data: { results: mixedResults } };
+  } catch (e) {
+    return { error: e.message };
+  }
+};
+
+async function fetchVisualResults(mode, gender, category, imageUrl, productId, filters) {
+  let endpoint = generateVisualBaseUrl(gender, category);
+  switch (mode) {
+    case 'url':
+      endpoint = addUrl(endpoint, imageUrl);
+      break;
+    case 'id':
+      endpoint = addId(endpoint, productId);
+      break;
+  }
+  endpoint = addFilterQueries(endpoint, filters);
+  return fetch(endpoint)
+    .then(response => response.json()
+      .then(json => json.docs || json.response
+  ));
+}
+
+async function fetchTextResults(mode, gender, category, tags, filters) {
+  if (mode === 'random' || !categoriesInfo['category'] || isEmpty(tags)) {
+    return [];
+  }
+  let endpoint = generateTextBaseUrl(gender, category);
+  endpoint = addTextQuery(endpoint, category, tags);
+  endpoint = addFilterQueries(endpoint, filters);
+  const numFound = fetch(endpoint)
+    .then(response => response.json()
+      .then(json => json.response.numFound
+  ));
+  endpoint += '&rows=' + await numFound;
+  return fetch(endpoint)
+    .then(response => response.json()
+      .then(json => json.response.docs
+  ));
+}
+
+const mixResults = (visualResults, textResults, category) => {
+  if (textResults.length) {
+    const maxScore = categoriesInfo[category].maxScore;
+    visualResults.forEach((visualResult) => {
+      const foundTextResult = textResults.find((textResult) => textResult.id === visualResult.id);
+      if (foundTextResult) {
+        visualResult.d = visualResult.d * (1.0 - 0.3 * foundTextResult.score / maxScore);
+      }
+    });
+    visualResults = visualResults.sort((a, b) => a.d - b.d);
+  }
+  return visualResults.map((r) => r.id).slice(0, 12);
+};
+
+const generateVisualBaseUrl = (gender, category) => {
+  return LIRESOLR_SERVER_URL + gender + '_' + category + '/lireq?rows=30&field=ce&ms=false';
+};
+
+const generateTextBaseUrl = (gender, category) => {
+  const threshold = (categoriesInfo[category].maxScore / 2) | 0; // double to int
+  return LIRESOLR_SERVER_URL + gender + '_' + category + '/select?wt=json&fl=id%20score&sc={!func}query({!type=lucene%20v=$q})&fq={!frange%20l=' + threshold + '}$sc';
+};
+
+const addFilterQueries = (url, filters) => {
+  let filterQueries = '';
+  if (filters.minPrice || filters.maxPrice) {
+    filterQueries += '&fq=price:[' + (filters.minPrice || '*') + '%20TO%20' + (filters.maxPrice || '*') + ']';
+  }
+  if (filters.shops && filters.shops.length) {
+    filterQueries += '&fq=shop:(';
+    filters.shops.forEach((shop) => filterQueries += shop + ' ');
+    filterQueries += ')';
+  }
+  return url + filterQueries;
+};
+
+const addUrl = (url, imageUrl) => {
+  const cloudinaryUrl = 'http://res.cloudinary.com/ddjzq70ve/image/fetch/x_0.1,y_0.1,w_0.8,h_0.8,c_crop,e_brightness_hsb:20/';
+
+  return url + '&url=' + cloudinaryUrl + imageUrl;
+};
+
+const addId = (url, productId) => {
+  return url + '&id=' + productId;
+};
+
+const addTextQuery = (url, category, tags) => {
+  let query = '&q='
+  const boosts = categoriesInfo[category].boosts;
+
+  for (const tagName in tags) {
+    query += tagName + ':' + tags[tagName] + '^=' + boosts[tagName] + ' ';
+  }
+
+  return url + query;
+};
